878d5e5a9392b2166d59fa038c3cdf28ed08065d
Aaro <aaro.heroja@gmail.com>
2022-09-03 20:17:27 +0300

    made fetch func with axios to get all recipes form backend

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,17 +1,31 @@
 import Head from "next/head";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Recipes = () => {
+    const [data, setData] = useState([])
+
+    const getRecipes = () => {
+        axios.get("/api/recipes/---placeholder---")
+            .then((res) => {
+                console.log(res);
+                setData(res.data);
+            })
+    }
+
+    useEffect(() => getRecipes(), [])
+
     return (
         <div>
             <Head>
                 <title>Recipes</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             </Head>
             <h1>
                 recipes
             </h1>
         </div>
 
-    )
+)
 }
 export default Recipes;
