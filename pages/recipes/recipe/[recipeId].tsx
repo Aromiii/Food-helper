b50345305391a74d1d6ae4b335d4f2bcfb26e5aa
Aaro <aaro.heroja@gmail.com>
2022-10-15 19:10:38 +0300

    made way to remove and see whole recipe

--- pages/recipes/recipe/[recipeId].tsx
+++ pages/recipes/recipe/[recipeId].tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import {useRouter} from "next/router";
-import {useState} from "react";
+import {db} from "../../../config/firebase";
+import {getDoc, doc, deleteDoc} from "@firebase/firestore";
+import {useEffect, useState} from "react";
 
 
 const RecipeSiteBody = (props: { data: { image: string; name: string; recipe: string; desc: string } }) => {
   return <>
-    <img src={props.data.image} className="object-cover h-56 w-screen" alt="Picture that user provided"/>
+    <img src={props.data.image}
+         className="object-cover h-56 w-screen"
+         alt="Picture that user provided"/>
     <div className="px-4 w-screen">
       <h1 className="my-1.5">
         {props.data.name}
@@ -21,8 +25,27 @@ const RecipeSiteBody = (props: { data: { image: string; name: string; recipe: st
 }
 
 const Recipe = () => {
+  const [data, setData] = useState({image: "", name: "Error", recipe: "", desc: ""})
   const router = useRouter()
-  const [data, setData] = useState({"image": "https://i.stack.imgur.com/6M513.png", "name": "Error: Data could not be retrieved from the database", "recipe": "Error", "desc": "Error"})
+
+  const docRef = doc(db, "recipes", router.query.recipeId)
+
+  //TODO migrate to getServerSideProps to make flash go away
+  useEffect(() => {
+    getDoc(docRef).then((doc) => {
+      console.log(doc.data(), doc.id)
+      setData(doc.data())
+    })
+  }, [])
+
+  const handleRemove = () => {
+    deleteDoc(docRef)
+      .then(() => {
+        location.replace("/recipes")
+      }).catch((error) => {
+        console.log(error)
+    })
+  }
 
   return (
     <div>
@@ -31,6 +54,11 @@ const Recipe = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
       <RecipeSiteBody data={data}/>
+      <button
+        className="bg-red-700 text-white border-black border-2 p-2 rounded-full m-3"
+        onClick={handleRemove}>
+        Remove {data.name}
+      </button>
     </div>
 
   )
