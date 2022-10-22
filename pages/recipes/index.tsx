05452543ebee88d5a614d67d8dbcea195a4b24b5
Aaro <aaro.heroja@gmail.com>
2022-10-22 12:44:31 +0300

    small design fixes

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -9,8 +9,8 @@ const RecipeContainer = (props: { data: DocumentData }) => {
 
   return (
     <Link href={"/recipes/recipe/" + props.data.id}>
-      <div className="recipeContainer">
-        <img src={props.data.image} className="recipeContainerImage"/>
+      <div className="bg-gray-500 m-2 rounded-2xl h-96 object-cover overflow-hidden">
+        <img src={props.data.image} className="rounded-2xl max-h-[60%] w-screen object-cover"/>
         <div className="p-2">
           <h1>{props.data.name}</h1>
           <p>{props.data.desc}</p>
